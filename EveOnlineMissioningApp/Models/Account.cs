222cee8b2402a9775dca43a0fb74c01b41ec18df
Matthew Lickei <mjlickei93@gmail.com>
2016-05-03 00:59:13 -0500

    initial commit

---
+++ EveOnlineMissioningApp/Models/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EveOnlineMissioningApp.Models
+{
+
+    public class Account
+    {
+
+        public int id { get; }
+
+    }
+
+}
---
+++ EveOnlineMissioningApp/Models/Mission.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EveOnlineMissioningApp.Models
+{
+
+    public class Mission
+    {
+
+        public int id { get; }
+        public DateTime startTime { get; set; }
+        public DateTime endTime { get; set; }
+        public string title { get; set; }
+
+    }
+
+}
